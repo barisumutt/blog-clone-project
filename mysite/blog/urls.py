5d11035323a4a1f5d6bc7be381d0254204e8e793
barisumut <barisumutbaykal@gmail.com>
2023-06-11 16:51:11 +0300

    Create comment_form.html, post_confirm_delete.html, post_detail.html, post_draft_list.html, post_form.html, post_list.html

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
+
+    path('/', views.PostListView.as_view(), name='post_list'),
     path('post/<pk>', views.PostDetailView.as_view(), name='post_detail' ),
 
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
