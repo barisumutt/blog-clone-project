a449a1d88212e3e03bd3e7ef2a65a9af717f9400
barisumut <barisumutbaykal@gmail.com>
2023-06-11 16:18:04 +0300

    Import LoginRequiredMixin, CreateView, UpdateView, DeleteView, PostForm, CommentForm, reverse_lazy to views.py Add CreatePostView, PostUpdateView, PostDeleteView to views.py and connection to urls.py

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('/', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
-    path('post/<pk>', views.PostDetailView.as_view(), name='post_detail' )
+    path('post/<pk>', views.PostDetailView.as_view(), name='post_detail' ),
 
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    path('post/<pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<pk>/remove', views.PostDeleteView.as_view(), name='post_remove')
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Post, Comment
-from django.views.generic import (TemplateView, ListView, DetailView)
+from .forms import PostForm, CommentForm
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (TemplateView, ListView, DetailView,
+                                  CreateView, UpdateView, DeleteView)
 
 
 class AboutView(TemplateView):
@@ -14,5 +18,25 @@ class PostListView(ListView):
     def get_queryset(self):
         return Post.objects.filter(published_date__lte=timezone.now.order_by('-published_date'))
 
+
 class PostDetailView(DetailView):
     model = Post
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
