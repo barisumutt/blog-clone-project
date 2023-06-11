0f15541bf4b3b0e6e2ef41ae7cb20589fb43d22a
barisumut <barisumutbaykal@gmail.com>
2023-06-11 17:27:48 +0300

    Create post_publish, add_comment_to_post, comment_approve, comment_remove functions to views.py and relevant connections to urls.py

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -57,9 +57,15 @@ class DraftListView(LoginRequiredMixin, ListView):
 
 ##################################################
 ##################################################
+@login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish
+    return redirect('post_detail', pk=pk)
+
 
 @login_required
-def add_comment_to_post(request,pk):
+def add_comment_to_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -68,6 +74,21 @@ def add_comment_to_post(request,pk):
             comment.post = post
             comment.save()
             return redirect('post_detail', pk=post.pk)
-        else:
-            form = CommentForm()
-        return render(request, 'blog/comment_form.html', {'form': form})
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
