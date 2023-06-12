b0dc3048b26a681841d79110911374a5331a6efb
barisumut <barisumutbaykal@gmail.com>
2023-06-12 13:14:15 +0300

    load static to base.html and edit urls

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
 
-    path('/', views.PostListView.as_view(), name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('post/<pk>', views.PostDetailView.as_view(), name='post_detail' ),
 
     path('post/new/', views.CreatePostView.as_view(), name='post_new'),
