0b44c0f56cc88675aae6ec062b8724c875d1bceb
barisumut <barisumutbaykal@gmail.com>
2023-06-12 13:33:06 +0300

    Fix base.html and login.html and urls.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for mysite project.
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
 from django.contrib.auth import views
-from django.contrib.auth import views as auth_views
+#from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
-    path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout')
+    path('', include('blog.urls')),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'}),
 ]
